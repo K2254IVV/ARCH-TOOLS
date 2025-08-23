06b08af80130e9c546a4b339733838907aec0b69
Тури Фурри <159616423+K2254IVV@users.noreply.github.com>
2025-08-23 08:36:04 +0000

    Create install-pim.sh

---
+++ files/ArchTools/PiM/1.0r/install-pim.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+clear
+echo "Installing ArchTools PiM!"
+mkdir -p /tmp/archtools-tmp
+cd /tmp/archtools-tmp
+switcherurl=$(wget -q -O - https://raw.githubusercontent.com/K2254IVV/ARCH-TOOLS/refs/heads/main/tools/switcher/lastest-pim | tr -d '\n')
+wget $switcherurl
+debiurl=$(wget -q -O - https://raw.githubusercontent.com/K2254IVV/ARCH-TOOLS/refs/heads/main/tools/deb-installer/lastest | tr -d '\n')
+wget $debiurl
+sudo chmod +x switcher
+sudo chmod +x deb-install
+echo "Installing Scripts to /bin..."
+sudo cp -Ri ./* /bin
+echo "ArchTools PiM Installed!"
+echo ""
+cd ~
