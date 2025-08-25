824194ccbc8c307766cf6b290d2c3780502aa956
Тури Фурри <159616423+K2254IVV@users.noreply.github.com>
2025-08-25 17:54:15 +0000

    Delete files directory

--- files/ArchTools/install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-lastest=$(curl -fsSL "https://raw.githubusercontent.com/K2254IVV/ARCH-TOOLS/refs/heads/main/files/ArchTools/lastest" | tr -d '[:space:]')
-sh -c "$(curl -fsSL "$lastest")"
